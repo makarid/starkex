de75233fbd5721e5a85f216309ed92be06ac879c
makarid <makarid@gmail.com>
2022-11-26 15:29:14 +0200

    Adding Goerlic network

--- src/main/java/com/fersoft/types/NetworkId.java
+++ src/main/java/com/fersoft/types/NetworkId.java
@@ -4,8 +4,8 @@ import java.math.BigInteger;
 
 public enum NetworkId {
     MAINNET(1, "a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",new BigInteger("02893294412a4c8f915f75892b395ebbf6859ec246ec365c3b1f56f47c3a0a5d",16)),
-    ROPSTEN(3, "8707a5bf4c2842d46b31a405ba41b858c0f876c4",new BigInteger("02c04d8b650f44092278a7cb1e1028c82025dff622db96c934b611b84cc8de5a",16));
-
+    ROPSTEN(3, "8707a5bf4c2842d46b31a405ba41b858c0f876c4",new BigInteger("02c04d8b650f44092278a7cb1e1028c82025dff622db96c934b611b84cc8de5a",16)),
+    GOERLIC(5, "F7a2fa2c2025fFe64427dd40Dc190d47ecC8B36e",new BigInteger("03bda2b4764039f2df44a00a9cf1d1569a83f95406a983ce4beb95791c376008",16));
     private final int id;
     private final String assetAddress;
     private final BigInteger collateralAddressId;
